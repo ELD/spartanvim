b0770c6126e93462fce4a216bea702830d21f052
Eric Dattore <eric@dattore.me>
2023-06-12 22:57:56 -0600

    Formatting

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -64,7 +64,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "gitcommit", "markdown "},
+  pattern = { "gitcommit", "markdown" },
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = false
