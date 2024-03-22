d761f9cc1d7a8f86821257a7b8fe11d56199e10e
Eric Dattore <eric.dattore@circleci.com>
2024-03-22 16:04:50 -0600

    Add some more autocmds

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -1,4 +1,11 @@
-vim.api.nvim_create_autocmd("VimEnter", {
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+local spartanvim_group = augroup("SpartaNvim", {})
+local yank_group = augroup("HighlightYank", {})
+
+autocmd("VimEnter", {
+	group = spartanvim_group,
 	callback = function()
 		if vim.env.ZELLIJ ~= nil then
 			vim.fn.system({ "zellij", "action", "switch-mode", "locked" })
@@ -6,10 +13,29 @@ vim.api.nvim_create_autocmd("VimEnter", {
 	end,
 })
 
-vim.api.nvim_create_autocmd("VimLeave", {
+autocmd("VimLeave", {
+	group = spartanvim_group,
 	callback = function()
 		if vim.env.ZELLIJ ~= nil then
 			vim.fn.system({ "zellij", "action", "switch-mode", "normal" })
 		end
 	end,
 })
+
+autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
+})
+
+autocmd({ "BufWritePre" }, {
+	group = spartanvim_group,
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
+})
+
