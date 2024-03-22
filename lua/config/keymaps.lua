97b03de5c7fa45f7e0382201c07f7349eb5e025c
Eric Dattore <eric.dattore@circleci.com>
2024-03-22 16:05:34 -0600

    wip keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,3 +15,17 @@ keymap({ "n", "i", "v" }, "<C-b>", "<cmd>NvimTreeToggle<cr>", { silent = true, d
 
 -- Noice dismiss
 keymap("n", "<leader>nd", "<cmd>NoiceDismiss<cr>", { desc = "Dismiss Noice Messages" })
+
+-- NOTE: Primeagen keymaps I'm deciphering...
+-- INFO: These two move lines up and down in visual mode
+keymap("v", "J", ":m '>+1<CR>gv=gv")
+keymap("v", "K", ":m '<-2<CR>gv=gv")
+
+-- NOTE: This removes the next line in normal mode
+keymap("n", "J", "mzJ`z")
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
+-- keymap("n", "n", "nzzzv")
+-- keymap("n", "N", "Nzzzv")
+
+keymap("x", "<leader>p", [["_dP]])
