6c64e381a67b16ff7d09ae09aea7aa6a672a0179
Eric Dattore <eric@dattore.me>
2024-03-23 09:29:29 -0600

    Add new keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,16 +16,25 @@ keymap({ "n", "i", "v" }, "<C-b>", "<cmd>NvimTreeToggle<cr>", { silent = true, d
 -- Noice dismiss
 keymap("n", "<leader>nd", "<cmd>NoiceDismiss<cr>", { desc = "Dismiss Noice Messages" })
 
--- NOTE: Primeagen keymaps I'm deciphering...
--- INFO: These two move lines up and down in visual mode
-keymap("v", "J", ":m '>+1<CR>gv=gv")
-keymap("v", "K", ":m '<-2<CR>gv=gv")
+keymap("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+keymap("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 
--- NOTE: This removes the next line in normal mode
-keymap("n", "J", "mzJ`z")
-keymap("n", "<C-d>", "<C-d>zz")
-keymap("n", "<C-u>", "<C-u>zz")
--- keymap("n", "n", "nzzzv")
--- keymap("n", "N", "Nzzzv")
+keymap("n", "J", "mzJ`z", { desc = "Remove next line" })
+keymap("n", "<C-d>", "<C-d>zz", { desc = "Move down a page and center" })
+keymap("n", "<C-u>", "<C-u>zz", { desc = "Move up a page and center" })
+keymap("n", "n", "nzzzv", { desc = "Next occurrence and center" })
+keymap("n", "N", "Nzzzv", { desc = "Previous occurrence and center" })
 
 keymap("x", "<leader>p", [["_dP]])
+keymap({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without storing" })
+
+keymap({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy selection to system clipboard" })
+keymap("n", "<leader>y", [["+Y]], { desc = "Copy line to system clipboard" })
+
+keymap("n", "<C-k>", "<cmd>cnext<CR>zz", { desc = "Next item in quickfix list" })
+keymap("n", "<C-j>", "<cmd>cnext<CR>zz", { desc = "Previous item in quickfix list" })
+keymap("n", "<leader>k", "<cmd>lnext<CR>zz", { desc = "Next item in location list" })
+keymap("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "Previous item in location list" })
+
+keymap("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Find and replace current word" })
+keymap("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "Make file executable", silent = true })
