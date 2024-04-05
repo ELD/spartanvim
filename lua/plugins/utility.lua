7571bb23afb51675b914ee8291191919f7c16d4e
Eric Dattore <eric.dattore@circleci.com>
2024-04-05 13:10:06 -0600

    Add Codesnap plugin

--- lua/plugins/utility.lua
+++ lua/plugins/utility.lua
@@ -143,5 +143,22 @@ return {
 				hop.hint_words({ direction = directions.BEFORE_CURSOR, hint_offset = 1 })
 			end, { remap = true, desc = "Hop word before cursor" })
 		end,
-	}
+	},
+	{
+		"mistricky/codesnap.nvim",
+		build = "make",
+		config = function()
+			require("codesnap").setup({
+				save_path = "~/workspace/codesnaps",
+				has_breadcrumbs = true,
+				bg_theme = "summer",
+				watermark = "ELD",
+				mac_window_bar = true,
+				code_font_family = "OperatorMonoSSmLig Nerd Font",
+			})
+
+			vim.keymap.set({ "n", "x" }, "<leader>cnc", [[CodeSnap]], { silent = true, desc = "CodeSnap to clipboard" })
+			vim.keymap.set({ "n", "x" }, "<leader>cns", [[CodeSnapSave]], { silent = true, desc = "CodeSnap to clipboard" })
+		end,
+	},
 }
