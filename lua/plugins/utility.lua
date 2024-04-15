932d4692f9390fddfb9c8366c6aecd48352d7a2f
Eric Dattore <eric@dattore.me>
2024-04-15 11:16:09 -0500

    Add undotree plugin

--- lua/plugins/utility.lua
+++ lua/plugins/utility.lua
@@ -161,4 +161,12 @@ return {
 			vim.keymap.set({ "n", "x" }, "<leader>cns", [[CodeSnapSave]], { silent = true, desc = "CodeSnap to clipboard" })
 		end,
 	},
+	{
+		"jiaoshijie/undotree",
+		dependencies = "nvim-lua/plenary.nvim",
+		config = true,
+		keys = { -- load the plugin only when using it's keybinding:
+			{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+		},
+	},
 }
