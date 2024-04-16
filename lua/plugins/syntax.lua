a4e5562c7b1953a10923238acbe46b699038c125
Eric Dattore <eric@dattore.me>
2024-04-16 14:27:56 -0600

    Add treesitter-context

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -1,38 +1,48 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-	},
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"windwp/nvim-ts-autotag",
+		},
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = {
-				"c",
-				"cpp",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"javascript",
-				"typescript",
-				"html",
-				"css",
-				"rust",
-				"go",
-				"terraform",
-				"tsx",
-			},
-			autopairs = { enable = true },
-			autotag = { enable = true },
-			sync_install = false,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = true,
-			},
-			indent = { enable = true },
-		})
-	end,
+			configs.setup({
+				ensure_installed = {
+					"c",
+					"cpp",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"javascript",
+					"typescript",
+					"html",
+					"css",
+					"rust",
+					"go",
+					"terraform",
+					"tsx",
+				},
+				autopairs = { enable = true },
+				autotag = { enable = true },
+				sync_install = false,
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = true,
+				},
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		config = function()
+			local ntc = require("treesitter-context")
+
+			ntc.setup()
+		end,
+	}
 }
 
