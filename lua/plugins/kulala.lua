27fc2f33ad1a417bcc9c624672f55df525c1f27d
Eric Dattore <eric@dattore.me>
2025-02-08 16:10:44 -0700

    Updates

---
+++ lua/plugins/kulala.lua
@@ -0,0 +1,7 @@
+return {
+	"mistweaverco/kulala.nvim",
+	opts = {},
+	config = function()
+		local map = vim.keymap.set
+	end,
+}
--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,13 +1,38 @@
 return {
-	"mrcjkb/rustaceanvim",
-	version = "^5", -- Recommended
-	lazy = false,   -- This plugin is already lazy
-	config = function()
-		local lsp_custom = require("utils.lsp_custom")
-		vim.g.rustaceanvim = {
-			server = {
-				on_attach = lsp_custom.on_attach,
-			},
-		}
-	end
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^5", -- Recommended
+		lazy = false, -- This plugin is already lazy
+		config = function()
+			local lsp_custom = require("utils.lsp_custom")
+			vim.g.rustaceanvim = {
+				server = {
+					on_attach = lsp_custom.on_attach,
+				},
+			}
+		end
+	},
+	{
+		"saecki/crates.nvim",
+		event = { "BufRead Cargo.toml" },
+		config = function()
+			local lsp_custom = require("utils.lsp_custom")
+			require("crates").setup({
+				lsp = {
+					enabled = true,
+					on_attach = lsp_custom.on_attach,
+					actions = true,
+					completion = true,
+					hover = true,
+				},
+				completion = {
+					crates = {
+						enabled = true,
+						max_results = 8,
+						min_chars = 3,
+					},
+				},
+			})
+		end,
+	}
 }
